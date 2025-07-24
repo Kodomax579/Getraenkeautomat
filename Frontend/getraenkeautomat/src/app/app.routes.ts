974048646f3134edeafb10d94448a43fdd810f53
Carsten Engelhardt <carsten.engelhardt@beumer.com>
2025-07-24 10:48:18 +0200

    Frontend: route to homepage after login

--- Frontend/getraenkeautomat/src/app/app.routes.ts
+++ Frontend/getraenkeautomat/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
+import { Login } from './components/pages/home/login';
 import { Home } from './components/pages/home/home';
 
 export const routes: Routes = [
-  {path:"", component:Home},
+  {path:"", component:Login},
+  {path:"Home", component:Home},
 ];
