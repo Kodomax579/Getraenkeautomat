2d0331ae22f8d9926edfda2218c514766b70e5e8
Carsten Engelhardt <carsten.engelhardt@beumer.com>
2025-07-25 15:49:06 +0200

    Backend: Create Lootbox service

---
+++ Backend/LootboxService/LootboxService/Controllers/LootboxController.cs
@@ -0,0 +1,25 @@
+﻿using LootboxService.Models;
+using LootboxService.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LootboxService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LootboxController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<Lootbox>> GetAll() => LootBoxService.GetAll();
+
+        [HttpGet("{id}")]
+        public ActionResult<int> Start(int id)
+        {
+            var lootbox = LootBoxService.Start(id);
+
+            if (lootbox == -1)
+                return NotFound("Lootbox not found!!!!!");
+
+            return lootbox;
+        }
+    }
+}
