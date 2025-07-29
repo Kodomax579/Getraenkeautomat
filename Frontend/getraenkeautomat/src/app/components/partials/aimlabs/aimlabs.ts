5f20ea79856d009712b6bf5b8cc60abc952c662a
David Mendel <david.mendel@beumer.com>
2025-07-29 15:43:50 +0200

    Frontend: error fix. Initialize random area later

--- Frontend/getraenkeautomat/src/app/components/partials/aimlabs/aimlabs.ts
+++ Frontend/getraenkeautomat/src/app/components/partials/aimlabs/aimlabs.ts
@@ -6,15 +6,15 @@ import {
   Output,
   signal,
   ViewChild,
-} from '@angular/core';
-import { Circle } from './circle/circle';
-import { Game } from '../../../services/game';
+} from "@angular/core";
+import { Circle } from "./circle/circle";
+import { Game } from "../../../services/game";
 
 @Component({
-  selector: 'app-aimlabs',
+  selector: "app-aimlabs",
   imports: [Circle],
-  templateUrl: './aimlabs.html',
-  styleUrl: './aimlabs.scss',
+  templateUrl: "./aimlabs.html",
+  styleUrl: "./aimlabs.scss",
 })
 export class Aimlabs {
   private gameService = inject(Game);
@@ -24,10 +24,10 @@ export class Aimlabs {
   public left = signal<number>(0);
 
   @Output() leave = new EventEmitter<boolean>();
-  @ViewChild('background') backgroundRef!: ElementRef;
+  @ViewChild("background") backgroundRef!: ElementRef<HTMLDivElement>;
 
-  ngOnInit(): void {
-    const backgroundEl = this.backgroundRef.nativeElement as HTMLElement;
+  ngAfterViewInit() {
+    const backgroundEl: HTMLDivElement = this.backgroundRef.nativeElement;
     const width: number = backgroundEl.clientWidth - 96;
     const height: number = backgroundEl.clientHeight - 96;
 
@@ -37,7 +37,7 @@ export class Aimlabs {
 
   onClick() {
     this.counter++;
-    this.ngOnInit();
+    this.ngAfterViewInit();
     if (this.counter % 5 === 0) {
       this.gameService.getCashAndLevel(0.1, 0.1);
     }
